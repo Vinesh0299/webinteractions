168edd49211d6eb31ef6f9bbfee916b55cd6350e
Katewa, Vinesh | Vinesh | RMI <vinesh.katewa@rakuten.com>
2023-01-31 17:20:17 +0900

    Added files for setup and testing all testcases with a single command

---
+++ test/test_all.py
@@ -0,0 +1,10 @@
+import unittest
+
+from test_http import HttpTestMethods
+from test_ip import IPTestMethods
+from test_tcp_socket import TCPSocketTestMethods
+from test_udp_socket import UDPSocketTestMethods
+from test_xmlrpc import XmlrpcTestMethods
+
+if __name__ == "__main__":
+    unittest.main()
