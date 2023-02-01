f7ad7ef087f9428797a8853b687e6f7cbe13572c
Katewa, Vinesh | Vinesh | RMI <vinesh.katewa@rakuten.com>
2023-02-01 16:35:34 +0900

    Added files and bash script that starts and runs the servers for testing

---
+++ test/setup_for_tests.sh
@@ -0,0 +1,5 @@
+python3 test/start_tcp_synchronous.py &
+python3 test/start_tcp_threaded.py &
+python3 test/start_udp_synchronous.py &
+python3 test/start_udp_threaded.py &
+python3 test/start_xmlrpc.py &
