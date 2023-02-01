ace807f30ceb15f7a0c26a2c6ac73e39bcb0aa66
Katewa, Vinesh | Vinesh | RMI <vinesh.katewa@rakuten.com>
2023-02-01 16:40:01 +0900

    Added functions to xmlrpc server side for testing

--- test/start_xmlrpc.py
+++ test/start_xmlrpc.py
@@ -1,3 +1,18 @@
 from webinteractions.server_client.xmlrpc_wrapper import create_xmlrpc_server
 
-create_xmlrpc_server()
+def add(a, b):
+    return a + b
+
+def mult(a, b):
+    return a * b
+
+def pow(a, b):
+    return a ** b
+
+func_map={
+    "add": add,
+    "mult": mult,
+    "pow": pow
+}
+
+create_xmlrpc_server(paths=("/RPC2",), function_map=func_map)
